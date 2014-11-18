07a9d1e2bba3291b8c1da4c44e9f45db68e490a5
Robert Gill <RobertGill@etech.net>
2014-11-18 13:38:11 +0000

    Added member expression tests

--- Expressions/Program.cs
+++ Expressions/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +13,38 @@ namespace Expressions
         {
             var expressions = new ExpressionTest();
             expressions.Run();
+
+            var memberInfo = new MemberInfoTest();
+            memberInfo.Run();
+        }
+    }
+
+    public class MemberInfoTest
+    {
+        public string TestProperty { get; set; }
+
+        public void Run()
+        {
+            var name = GetMemberName(() => TestProperty);
+            Console.WriteLine("PropertyName:{0}", name);
+
+            var propertyInfo = GetType().GetProperty(name);
+            propertyInfo.SetValue(this, "Test Data");
+
+            Console.WriteLine(TestProperty);
+
+            Console.ReadKey();
         }
 
-      
+        private string GetMemberName<T>(Expression<Func<T>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression != null)
+            {
+                return memberExpression.Member.Name;
+            }
+
+            return "Error";
+        }
     }
 }
