1c28233fdf39faeb6f001fd328e84cff9e3a84dd
Robert Gill <RobertGill@etech.net>
2014-11-18 13:14:09 +0000

    Added expression/func basic tests

---
+++ Expressions/ExpressionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Expressions
+{
+    public class ExpressionTest
+    {
+        public void Run()
+        {
+            var test = new Func<string>(() => "test");
+            Console.WriteLine("Test:{0}", test());
+
+            var funcData = Func(() => "Func<string> data!");
+            Console.WriteLine("Func:{0}",funcData);
+
+            var expressionData = Expression(() => "Expression<Func<string>> data!");
+            Console.WriteLine("Expression:{0}", expressionData);
+
+            Console.ReadKey();
+        }
+
+        private string Expression(Expression<Func<string>> expression)
+        {
+            var func = expression.Compile();
+            return func();
+        }
+
+        private string Func(Func<string> test)
+        {
+            return test();
+        }
+    }
+}
